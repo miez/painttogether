831accac5a8f18be4922fd023dee9c5aaac17b2b
martinblankenstein <martinblankenstein@cecc7784-a7e1-8231-3aa4-aa32d0e6d071>
2010-08-11 12:58:15 +0000

    PtPortListener im Serverprojekt fertig gestellt

---
+++ PaintTogetherServer/PaintTogetherServer.Test/Adapter/PtPortListenerCS/ProcessStartPortListingMessageTest.cs
@@ -0,0 +1,64 @@
+﻿
+/* $HeadURL: http://ws201075/svn/deg/dotNet/trunk/Pegasus/src/HostApplicationForm.cs $
+-----------------------------------------------------------------------------
+        (c) by Martin Blankenstein
+
+Dieses Dokument und die hierin enthaltenen Informationen unterliegen
+dem Urheberrecht und duerfen ohne die schriftliche Genehmigung des
+Herausgebers weder als ganzes noch in Teilen dupliziert oder reproduziert
+noch manipuliert werden.
+
+-----------------+------------------------------------------------------------
+Version          : $Revision: 450 $
+-----------------+------------------------------------------------------------
+Last Change      : $LastChangedDate: 2009-02-23 18:26:54 +0100 (Mo, 23 Feb 2009) $
+-----------------+------------------------------------------------------------
+Last User        : $LastChangedBy: NLBERLIN\mblankenstein $
+-----------------+------------------------------------------------------------
+Beschreibung     :
+-----------------+ 
+
+-----------------+------------------------------------------------------------
+Updates          :
+-----------------+
+
+$Id: HostApplicationForm.cs 450 2009-02-23 17:26:54Z NLBERLIN\mblankenstein $
+
+*/
+
+using System.Net;
+using System.Net.Sockets;
+using NUnit.Framework;
+using System.Collections.Generic;
+using PaintTogetherServer.Adapter;
+using PaintTogetherServer.Messages.Adapter;
+using System.Threading;
+
+namespace PaintTogetherServer.Test.Adapter.PtPortListenerCS
+{
+    [TestFixture]
+    public class ProcessStartPortListingMessageTest
+    {
+        [Test]
+        public void mehrere_Verbindungen_aufbauen()
+        {
+            var serverPort = 34523;
+            var newConCount = 0;
+
+            var listener = new PtPortListener();
+            listener.ProcessStartPortListingMessage(new StartPortListingMessage { Port = serverPort });
+            listener.OnNewConnection += message => newConCount++;
+
+            for (var i = 0; i < 20; i++)
+            {
+                // Verbindung zu dem überwachten Port aufbauen
+                var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                clientSocket.Connect(Dns.GetHostAddresses("192.168.2.44")[0], serverPort);
+            }
+
+            Thread.Sleep(500);
+
+            Assert.That(newConCount, Is.EqualTo(20));
+        }
+    }
+}
---
+++ PaintTogetherServer/PaintTogetherServer/Adapter/PtPortListener.cs
@@ -0,0 +1,91 @@
+﻿/* $HeadURL: http://ws201075/svn/deg/dotNet/trunk/Pegasus/src/HostApplicationForm.cs $
+-----------------------------------------------------------------------------
+        (c) by Martin Blankenstein
+
+Dieses Dokument und die hierin enthaltenen Informationen unterliegen
+dem Urheberrecht und duerfen ohne die schriftliche Genehmigung des
+Herausgebers weder als ganzes noch in Teilen dupliziert oder reproduziert
+noch manipuliert werden.
+
+-----------------+------------------------------------------------------------
+Version          : $Revision: 450 $
+-----------------+------------------------------------------------------------
+Last Change      : $LastChangedDate: 2009-02-23 18:26:54 +0100 (Mo, 23 Feb 2009) $
+-----------------+------------------------------------------------------------
+Last User        : $LastChangedBy: NLBERLIN\mblankenstein $
+-----------------+------------------------------------------------------------
+Beschreibung     :
+-----------------+ 
+
+-----------------+------------------------------------------------------------
+Updates          :
+-----------------+
+
+$Id: HostApplicationForm.cs 450 2009-02-23 17:26:54Z NLBERLIN\mblankenstein $
+
+*/
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using PaintTogetherServer.Contracts.Adapter;
+using PaintTogetherServer.Messages.Adapter;
+using PaintTogetherServer.Messages.Adapter.ConnectionManager;
+
+namespace PaintTogetherServer.Adapter
+{
+    /// <summary>
+    /// Startet die Überwachung eines Ports und meldet alle eingehenden Verbindungen
+    /// </summary>
+    internal class PtPortListener : IPtPortListener
+    {
+        /// <summary>
+        /// Informiert über eine neu eingegangene Verbindung
+        /// </summary>
+        public event Action<NewConnectionMessage> OnNewConnection;
+
+        /// <summary>
+        /// Verarbeitet die initiale Aufforderung den Apdater zu starten und
+        /// einen Port zu überwachen
+        /// </summary>
+        /// <param name="message"></param>
+        public void ProcessStartPortListingMessage(StartPortListingMessage message)
+        {
+            // Überwachung wird in anderem Thread gestartet, da sonst der
+            // Hauptthread blockieren würde
+            var thread = new Thread(StartListing);
+            thread.Start(message.Port);
+
+            // TODO listener.Stop ! Extra Event! 
+            // (auch wenn durch Shutdown der Anwendung ein Stop automatisch erfolgt)
+        }
+
+        private void StartListing(object port)
+        {
+            // Auf clientverbindung lauschen
+            var listener = new TcpListener(IPAddress.Any, (int)port);
+            listener.Start();
+
+            while (true) // TODO Stopbedingung
+            {
+                // Den durch Connect gestarteten Verbindungsaufbau akzeptieren
+                while (!listener.Pending())
+                {
+                    Thread.Sleep(100);
+                }
+
+                // In einem separten Thread die neue Verbindung übergeben,
+                // damit der Empfang weiterer Verbindungen nicht gestört wird
+                var serverSocket = listener.AcceptSocket();
+                var thread = new Thread(SignalNewConnection);
+                thread.Start(serverSocket);
+            }
+        }
+
+        private void SignalNewConnection(object socket)
+        {
+            OnNewConnection(new NewConnectionMessage { Socket = socket as Socket });
+        }
+    }
+}
