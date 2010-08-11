9807102e9a03e0449b76a8c6cfa2066810fcf534
martinblankenstein <martinblankenstein@cecc7784-a7e1-8231-3aa4-aa32d0e6d071>
2010-08-11 19:05:43 +0000

    Messages-Klassen für PaintTogetherClient.Messages fertiggestellt

---
+++ PaintTogetherClient/PaintTogetherClient.Messages/Core/PaintContentManager/InitPaintManagerMessage.cs
@@ -0,0 +1,45 @@
+﻿/* $HeadURL: http://ws201075/svn/deg/dotNet/trunk/Pegasus/src/HostApplicationForm.cs $
+-----------------------------------------------------------------------------
+        (c) by Martin Blankenstein
+
+Dieses Dokument und die hierin enthaltenen Informationen unterliegen
+dem Urheberrecht und duerfen ohne die schriftliche Genehmigung des
+Herausgebers weder als ganzes noch in Teilen dupliziert oder reproduziert
+noch manipuliert werden.
+
+-----------------+------------------------------------------------------------
+Version          : $Revision: 450 $
+-----------------+------------------------------------------------------------
+Last Change      : $LastChangedDate: 2009-02-23 18:26:54 +0100 (Mo, 23 Feb 2009) $
+-----------------+------------------------------------------------------------
+Last User        : $LastChangedBy: NLBERLIN\mblankenstein $
+-----------------+------------------------------------------------------------
+Beschreibung     :
+-----------------+ 
+
+-----------------+------------------------------------------------------------
+Updates          :
+-----------------+
+
+$Id: HostApplicationForm.cs 450 2009-02-23 17:26:54Z NLBERLIN\mblankenstein $
+
+*/
+
+namespace PaintTogetherClient.Messages.Core.PaintContentManager
+{
+    /// <summary>
+    /// Initiale Nachricht für die Malbereichsverwaltung
+    /// </summary>
+    internal class InitPaintManagerMessage
+    {
+        /// <summary>
+        /// Höhe des Malbereichs
+        /// </summary>
+        public int Height { get; set; }
+        
+        /// <summary>
+        /// Breite des Malbereichs
+        /// </summary>
+        public int Width { get; set; }
+    }
+}
---
+++ PaintTogetherClient/PaintTogetherClient.Messages/Portal/PaintSelfMessage.cs
@@ -0,0 +1,47 @@
+﻿/* $HeadURL: http://ws201075/svn/deg/dotNet/trunk/Pegasus/src/HostApplicationForm.cs $
+-----------------------------------------------------------------------------
+        (c) by Martin Blankenstein
+
+Dieses Dokument und die hierin enthaltenen Informationen unterliegen
+dem Urheberrecht und duerfen ohne die schriftliche Genehmigung des
+Herausgebers weder als ganzes noch in Teilen dupliziert oder reproduziert
+noch manipuliert werden.
+
+-----------------+------------------------------------------------------------
+Version          : $Revision: 450 $
+-----------------+------------------------------------------------------------
+Last Change      : $LastChangedDate: 2009-02-23 18:26:54 +0100 (Mo, 23 Feb 2009) $
+-----------------+------------------------------------------------------------
+Last User        : $LastChangedBy: NLBERLIN\mblankenstein $
+-----------------+------------------------------------------------------------
+Beschreibung     :
+-----------------+ 
+
+-----------------+------------------------------------------------------------
+Updates          :
+-----------------+
+
+$Id: HostApplicationForm.cs 450 2009-02-23 17:26:54Z NLBERLIN\mblankenstein $
+
+*/
+
+using System.Drawing;
+
+namespace PaintTogetherClient.Messages.Portal
+{
+    /// <summary>
+    /// Malanfrage
+    /// </summary>
+    public class PaintSelfMessage
+    {
+        /// <summary>
+        /// Zu bemaltender Punkt
+        /// </summary>
+        public Point Point { get; set; }
+
+        /// <summary>
+        /// Malfarbe
+        /// </summary>
+        public Color Color { get; set; }
+    }
+}
