518182eef759236abc07b5cf94b22215b4acbfb1
martinblankenstein <martinblankenstein@cecc7784-a7e1-8231-3aa4-aa32d0e6d071>
2010-08-11 16:45:02 +0000

    PtPaintFieldManager fertiggestellt

---
+++ PaintTogetherServer/PaintTogetherServer.Test/Core/PtPaintFieldManagerCS/ProcessPaintMessageTest.cs
@@ -0,0 +1,83 @@
+﻿
+/* $HeadURL: http://ws201075/svn/deg/dotNet/trunk/Pegasus/src/HostApplicationForm.cs $
+-----------------------------------------------------------------------------
+        (c) by Martin Blankenstein
+
+Dieses Dokument und die hierin enthaltenen Informationen unterliegen
+dem Urheberrecht und duerfen ohne die schriftliche Genehmigung des
+Herausgebers weder als ganzes noch in Teilen dupliziert oder reproduziert
+noch manipuliert werden.
+
+-----------------+------------------------------------------------------------
+Version          : $Revision: 450 $
+-----------------+------------------------------------------------------------
+Last Change      : $LastChangedDate: 2009-02-23 18:26:54 +0100 (Mo, 23 Feb 2009) $
+-----------------+------------------------------------------------------------
+Last User        : $LastChangedBy: NLBERLIN\mblankenstein $
+-----------------+------------------------------------------------------------
+Beschreibung     :
+-----------------+ 
+
+-----------------+------------------------------------------------------------
+Updates          :
+-----------------+
+
+$Id: HostApplicationForm.cs 450 2009-02-23 17:26:54Z NLBERLIN\mblankenstein $
+
+*/
+
+using System.Drawing;
+using NUnit.Framework;
+using PaintTogetherServer.Core;
+using PaintTogetherServer.Messages.Core.PaintFieldManager;
+using PaintTogetherServer.Messages.Adapter;
+
+namespace PaintTogetherServer.Test.Core.PtPaintFieldManagerCS
+{
+    [TestFixture]
+    public class ProcessPaintMessageTest
+    {
+        private PtPaintFieldManager _fieldManager;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fieldManager = new PtPaintFieldManager();
+            _fieldManager.ProcessInitMessage(new InitMessage { Height = 324, Width = 123 });
+
+            var request = new GetCurrentPaintContentRequest();
+            _fieldManager.ProcessGetCurrentPaintContentRequest(request);
+        }
+
+        [Test]
+        public void Einen_Punkt_bemalen_Benachrichtigung_pruefen()
+        {
+            NotifyPaintToClientsMessage paintedMessage = null;
+            _fieldManager.OnNotifyPaint += message => paintedMessage = message;
+
+            var point = new Point(3, 12);
+            var color = Color.DodgerBlue;
+
+            _fieldManager.ProcessClientPainted(new ClientPaintedMessage { Color = color, Point = point });
+
+            Assert.That(paintedMessage.Point, Is.EqualTo(point));
+            Assert.That(paintedMessage.Color, Is.EqualTo(color));
+        }
+
+        [Test]
+        public void Einen_Punkt_bemalen_Malinhalt_pruefen()
+        {
+            _fieldManager.OnNotifyPaint += message => Assert.True(true); /* Dummyverdrahtung damit keine NRE auftritt*/
+
+            var point = new Point(42, 23);
+            var color = Color.FromArgb(42,143,123);
+
+            _fieldManager.ProcessClientPainted(new ClientPaintedMessage { Color = color, Point = point });
+
+            var request = new GetCurrentPaintContentRequest();
+            _fieldManager.ProcessGetCurrentPaintContentRequest(request);
+
+            Assert.That(request.Result.GetPixel(point.X, point.Y), Is.EqualTo(color));
+        }
+    }
+}
---
+++ PaintTogetherServer/PaintTogetherServer/Core/PtPaintFieldManager.cs
@@ -0,0 +1,116 @@
+﻿/* $HeadURL: http://ws201075/svn/deg/dotNet/trunk/Pegasus/src/HostApplicationForm.cs $
+-----------------------------------------------------------------------------
+        (c) by Martin Blankenstein
+
+Dieses Dokument und die hierin enthaltenen Informationen unterliegen
+dem Urheberrecht und duerfen ohne die schriftliche Genehmigung des
+Herausgebers weder als ganzes noch in Teilen dupliziert oder reproduziert
+noch manipuliert werden.
+
+-----------------+------------------------------------------------------------
+Version          : $Revision: 450 $
+-----------------+------------------------------------------------------------
+Last Change      : $LastChangedDate: 2009-02-23 18:26:54 +0100 (Mo, 23 Feb 2009) $
+-----------------+------------------------------------------------------------
+Last User        : $LastChangedBy: NLBERLIN\mblankenstein $
+-----------------+------------------------------------------------------------
+Beschreibung     :
+-----------------+ 
+
+-----------------+------------------------------------------------------------
+Updates          :
+-----------------+
+
+$Id: HostApplicationForm.cs 450 2009-02-23 17:26:54Z NLBERLIN\mblankenstein $
+
+*/
+
+using System;
+using PaintTogetherServer.Contracts.Core;
+using PaintTogetherServer.Messages.Adapter;
+using PaintTogetherServer.Messages.Core.PaintFieldManager;
+using System.Drawing;
+using log4net;
+
+namespace PaintTogetherServer.Core
+{
+    /// <summary>
+    /// Verwaltet den Malbereich in Form einer BitMap
+    /// </summary>
+    internal class PtPaintFieldManager : IPtPaintFieldManager
+    {
+        /// <summary>
+        /// Beauftragt die Benachrichtigung aller Clients über
+        /// einen neu bemalten Punkt
+        /// </summary>
+        public event Action<NotifyPaintToClientsMessage> OnNotifyPaint;
+
+        /// <summary>
+        /// Der aktuelle Malinhalt
+        /// </summary>
+        private Bitmap _paintContent;
+
+        /// <summary>
+        /// log4net-Logger für Logging
+        /// </summary>
+        private static ILog Log
+        {
+            get
+            {
+                return LogManager.GetLogger("PtPaintFieldManager");
+            }
+        }
+
+        /// <summary>
+        /// Verarbeitet die Initialisierungsaufforderung
+        /// </summary>
+        /// <param name="message"></param>
+        public void ProcessInitMessage(InitMessage message)
+        {
+            Log.DebugFormat("Malbereich wird mit einer Größe von 'W={0}:H={1}' initialisiert", message.Width, message.Height);
+            _paintContent = new Bitmap(message.Width, message.Height);
+        }
+
+        /// <summary>
+        /// Setzt den aktuellen Malbereich im aktuellen Zustand in
+        /// das Result der Anfrage
+        /// </summary>
+        /// <param name="request"></param>
+        public void ProcessGetCurrentPaintContentRequest(GetCurrentPaintContentRequest request)
+        {
+            Log.Debug("Aktueller Malbereich wird abgefragt");
+            if (_paintContent == null)
+            {
+                Log.Error("Malbereich noch nicht initialisiert - Malbereich ist leer");
+                request.Result = new Bitmap(0, 0);
+                return;
+            }
+
+            // Nicht das Orginalbild weitergeben, da es verändert werden
+            // könnte
+            request.Result = _paintContent.Clone() as Bitmap;
+        }
+
+        /// <summary>
+        /// Verarbeitet die Anfrage zum Malen eines Beteiligten
+        /// </summary>
+        /// <param name="message"></param>
+        public void ProcessClientPainted(ClientPaintedMessage message)
+        {
+            Log.DebugFormat("Neue Malanfrage für Punkt 'X={0}:Y={1}'", message.Point.X, message.Point.Y);
+            if (_paintContent == null)
+            {
+                Log.Error("Malbereich noch nicht initialisiert - Malvorgang abgebrochen");
+                return;
+            }
+
+            // Ob der Punkt in der Malerei liegt muss hier nicht geprüft werden,
+            // da der Client nur Pixel bemalt, die innerhalb der Malerei vorhanden sind
+            // sollte hier doch mal ein Fehler auftreten werden die Testcases erweitert und
+            // die Prüfung hier eingebaut - CCD:YAGNI - you ain't gonna need it
+            _paintContent.SetPixel(message.Point.X, message.Point.Y, message.Color);
+
+            OnNotifyPaint(new NotifyPaintToClientsMessage { Color = message.Color, Point = message.Point });
+        }
+    }
+}
