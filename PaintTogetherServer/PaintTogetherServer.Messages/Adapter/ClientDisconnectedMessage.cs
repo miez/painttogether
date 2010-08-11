6dff2c8b60ebf945092bbf44d930cdd284a7f606
martinblankenstein <martinblankenstein@cecc7784-a7e1-8231-3aa4-aa32d0e6d071>
2010-08-11 12:00:10 +0000

    PaintTogetherServer - Messages und die oberen Contracts erstellt

---
+++ PaintTogetherServer/PaintTogetherServer.Messages/Adapter/ClientDisconnectedMessage.cs
@@ -0,0 +1,45 @@
+﻿/* $HeadURL: http://ws201075/svn/deg/dotNet/trunk/Pegasus/src/HostApplicationForm.cs $
+-----------------------------------------------------------------------------
+        (c) by Martin Blankenstein
+
+Dieses Dokument und die hierin enthaltenen Informationen unterliegen
+dem Urheberrecht und duerfen ohne die schriftliche Genehmigung des
+Herausgebers weder als ganzes noch in Teilen dupliziert oder reproduziert
+noch manipuliert werden.
+
+-----------------+------------------------------------------------------------
+Version          : $Revision: 450 $
+-----------------+------------------------------------------------------------
+Last Change      : $LastChangedDate: 2009-02-23 18:26:54 +0100 (Mo, 23 Feb 2009) $
+-----------------+------------------------------------------------------------
+Last User        : $LastChangedBy: NLBERLIN\mblankenstein $
+-----------------+------------------------------------------------------------
+Beschreibung     :
+-----------------+ 
+
+-----------------+------------------------------------------------------------
+Updates          :
+-----------------+
+
+$Id: HostApplicationForm.cs 450 2009-02-23 17:26:54Z NLBERLIN\mblankenstein $
+
+*/
+
+namespace PaintTogetherServer.Messages.Adapter
+{
+    /// <summary>
+    /// Signalisierung über eine verlorene Verbindung
+    /// </summary>
+    public class ClientDisconnectedMessage
+    {
+        /// <summary>
+        /// Alias der alten verbundenen Person
+        /// </summary>
+        public string Alias { get; set; }
+
+        /// <summary>
+        /// Malfarbe des ehemaligen Beteiligten
+        /// </summary>
+        public string Color { get; set; }
+    }
+}
